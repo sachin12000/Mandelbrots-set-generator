7ad5f6a0c3a92f96e9b515ffdcf34617e26336d5
Sachin Samarasinghe <samarya@mcmaster.ca>
2019-08-29 01:30:45 -0400

    added project

---
+++ FractalLockBits/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace FractalLockBits
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShowPalatte = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblCoordinates = new System.Windows.Forms.Label();
+            this.btnDrawOriginal = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.lblZoom = new System.Windows.Forms.Label();
+            this.btnFullscreen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBoxColoringMethods = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnShowControls = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnShowPalatte
+            // 
+            this.btnShowPalatte.Location = new System.Drawing.Point(115, 619);
+            this.btnShowPalatte.Name = "btnShowPalatte";
+            this.btnShowPalatte.Size = new System.Drawing.Size(96, 40);
+            this.btnShowPalatte.TabIndex = 0;
+            this.btnShowPalatte.Text = "Draw color palatte";
+            this.btnShowPalatte.UseVisualStyleBackColor = true;
+            this.btnShowPalatte.Click += new System.EventHandler(this.btnShowPalatte_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // lblCoordinates
+            // 
+            this.lblCoordinates.AutoSize = true;
+            this.lblCoordinates.Location = new System.Drawing.Point(541, 624);
+            this.lblCoordinates.Name = "lblCoordinates";
+            this.lblCoordinates.Size = new System.Drawing.Size(137, 65);
+            this.lblCoordinates.TabIndex = 2;
+            this.lblCoordinates.Text = "Screen coordinates\r\nX = 0  |  Y = 0\r\n\r\nComplex  plane coordinates\r\nX = 0  |  Im =" +
+                " 0";
+            // 
+            // btnDrawOriginal
+            // 
+            this.btnDrawOriginal.Location = new System.Drawing.Point(12, 619);
+            this.btnDrawOriginal.Name = "btnDrawOriginal";
+            this.btnDrawOriginal.Size = new System.Drawing.Size(97, 40);
+            this.btnDrawOriginal.TabIndex = 3;
+            this.btnDrawOriginal.Text = "Genrate Madelbrot set";
+            this.btnDrawOriginal.UseVisualStyleBackColor = true;
+            this.btnDrawOriginal.Click += new System.EventHandler(this.btnDrawOriginal_Click);
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Location = new System.Drawing.Point(115, 665);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(96, 40);
+            this.btnUndo.TabIndex = 4;
+            this.btnUndo.Text = "Undo";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // lblZoom
+            // 
+            this.lblZoom.AutoSize = true;
+            this.lblZoom.Location = new System.Drawing.Point(541, 689);
+            this.lblZoom.Name = "lblZoom";
+            this.lblZoom.Size = new System.Drawing.Size(46, 13);
+            this.lblZoom.TabIndex = 5;
+            this.lblZoom.Text = "Zoom = ";
+            // 
+            // btnFullscreen
+            // 
+            this.btnFullscreen.Location = new System.Drawing.Point(13, 665);
+            this.btnFullscreen.Name = "btnFullscreen";
+            this.btnFullscreen.Size = new System.Drawing.Size(96, 40);
+            this.btnFullscreen.TabIndex = 6;
+            this.btnFullscreen.Text = "Fullscreen";
+            this.btnFullscreen.UseVisualStyleBackColor = true;
+            this.btnFullscreen.Click += new System.EventHandler(this.btnFullscreen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 619);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Coloring method";
+            // 
+            // cmbBoxColoringMethods
+            // 
+            this.cmbBoxColoringMethods.FormattingEnabled = true;
+            this.cmbBoxColoringMethods.Items.AddRange(new object[] {
+            "Iterations With Smoothing",
+            "Red Stripes"});
+            this.cmbBoxColoringMethods.Location = new System.Drawing.Point(319, 635);
+            this.cmbBoxColoringMethods.Name = "cmbBoxColoringMethods";
+            this.cmbBoxColoringMethods.Size = new System.Drawing.Size(146, 21);
+            this.cmbBoxColoringMethods.TabIndex = 8;
+            this.cmbBoxColoringMethods.Text = "Iterations with smoothing";
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(217, 619);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(96, 40);
+            this.btnRefresh.TabIndex = 9;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnShowControls
+            // 
+            this.btnShowControls.Location = new System.Drawing.Point(217, 665);
+            this.btnShowControls.Name = "btnShowControls";
+            this.btnShowControls.Size = new System.Drawing.Size(96, 40);
+            this.btnShowControls.TabIndex = 10;
+            this.btnShowControls.Text = "Show Keyboard Controls";
+            this.btnShowControls.UseVisualStyleBackColor = true;
+            this.btnShowControls.Click += new System.EventHandler(this.btnShowControls_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 709);
+            this.Controls.Add(this.btnShowControls);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cmbBoxColoringMethods);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFullscreen);
+            this.Controls.Add(this.lblZoom);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnDrawOriginal);
+            this.Controls.Add(this.lblCoordinates);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnShowPalatte);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Madelbort Set";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShowPalatte;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblCoordinates;
+        private System.Windows.Forms.Button btnDrawOriginal;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.Label lblZoom;
+        private System.Windows.Forms.Button btnFullscreen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbBoxColoringMethods;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnShowControls;
+    }
+}
+
---
+++ FractalLockBits/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FractalLockBits
+{
+    public partial class Form1 : Form
+    {
+        const int MAX_ITERATIONS = 100;
+        bool fullscreen = false;
+        ulong zoom;  //zoom history
+        ulong[] zoomHistory = new ulong[100];  //zoom history for 100 steps 
+        double xBound, yBound;  //xBound is the left most x value of the graph, yBound is the upper most y value of the graph
+        double[] xBoundHistory = new double[100];  //History of the xBound
+        double[] yBoundHistory = new double[100];  //History of the yBound
+        sbyte offset = 0; //offset to the current step from the begenning (0) in history arrays 
+        double realSlope, imaginarySlope;  //Slopes for linear transformation equations
+        int width, height;
+
+        int[] colorArray = new int[768];  //Array of predefined colors(gradient). [Color #, BGRA]. 1 pixel takes 4 bytes in memory
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private unsafe void Form1_Load(object sender, EventArgs e)
+        {
+            zoom = 1;
+            xBound = -2.5d;
+            yBound = 1.5d;
+            width = pictureBox1.Width;  //Caching
+            height = pictureBox1.Height;  //Caching
+            realSlope = 4.0d / zoom / width; imaginarySlope = -3.0d / zoom / height;  //Caching
+            byte* colorArrayPtr;  //Pointer that will point to the first byte of coloarArray
+
+            //Generating the gradient and storing it in colorArray
+            byte colorValueR = 0;  //R
+            byte colorValueG = 0;  //G
+            byte colorValueB = 0;  //B
+
+            fixed (int* colorArrayPtrInt = colorArray)  //Temperory pointer has to be used inorder to cast int pointer to byte pointer
+            {
+                colorArrayPtr = (byte*)colorArrayPtrInt;
+            }
+
+
+            for (int i = 0; i < 768; i++)
+            {
+                if (i >= 512)
+                {
+                    colorValueR = (byte)(i - 512);
+                    colorValueG = (byte)(255 - colorValueR);
+                }
+                else if (i >= 256)
+                {
+                    colorValueG = (byte)(i - 256);
+                    colorValueB = (byte)(255 - colorValueG);
+                }
+                else
+                {
+                    colorValueB = (byte)(i);
+                }
+
+                colorArrayPtr[i * 4 + 3] = 255;  //A
+                colorArrayPtr[i * 4 + 2] = colorValueR;  //R
+                colorArrayPtr[i * 4 + 1] = colorValueG;  //G
+                colorArrayPtr[i * 4] = colorValueB;  //B
+            }
+        }
+
+        private unsafe Bitmap drawFractal()
+        {            byte coloringMethod = 1;
+
+            if (cmbBoxColoringMethods.Text == "Red Stripes")
+            {
+                coloringMethod = 2;
+            }
+
+            lblZoom.Text = "Zoom = " + zoom.ToString();
+            Bitmap fractalBMP = new Bitmap(width, height);
+            BitmapData fractalBMPData = fractalBMP.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.WriteOnly, fractalBMP.PixelFormat);  //Locking out the entire all the memory the Bitmap occupies inorder to direct editing
+            int* pointerToFirstPixel = (int*)(void*)fractalBMPData.Scan0.ToPointer();  //Pointer that points to the first byte of the Bitmap
+
+            double zr, zi, cr, ci, zrPrevious;  //Variales that will hold zReal, zImaginary, cReal, cImaginary, zRealPrevious
+            int iterations, colorIndex;  //Variables that will hold the # of iterations and an index value that refers to a color in the colors array
+            double modulus, mu;  //varialble to hold modulus of z. mu is used to decide the color
+
+            realSlope = 4.0d / zoom / width;  //Setting the real slope
+            imaginarySlope = -3.0d / zoom / height;  //Setting the imaginary slope
+
+            zr = zi = cr = ci = 0;  //Assigning an intial values to prevent errors
+            colorIndex = iterations = 0;  //Assigning ab intial values to prevent errors
+            modulus = mu = 0;  //Assigning an intial values to prevent errors
+
+            double maxR = 0, maxI = 0;
+
+            byte[] RGBA = new byte[4];
+            RGBA[0] = RGBA[1] = RGBA[2] = 0;
+            RGBA[3] = 255;  //Alpha = 255
+            //I tried to reduce the number of calls made to outside functions to improve efficiency. Therefore Math.Pow is not used for squaring.
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    cr = realSlope * x + xBound;
+                    ci = imaginarySlope * y + yBound;
+
+                    zr = 0; zi = 0;
+
+                    iterations = 0;
+                    colorIndex = 0;
+
+                    do
+                    {
+                        zrPrevious = zr; //Storing zReal in zRealPrevious
+                        zr = (zr * zr) - (zi * zi) + cr;  //Caculating the real part of z
+                        zi = (2 * zrPrevious * zi) + ci;  //Calculating the imaginary part of z
+                        iterations++;
+                    } while (Math.Sqrt((zr * zr) + (zi * zi)) < 2 && iterations < MAX_ITERATIONS);  //Checking absolute of z and iterations
+
+                    modulus = Math.Sqrt(zr * zr + zi + Math.Pow(zi, 2));  //Calculating absolute of z
+
+                    if (coloringMethod == 1)  //Selecting which coloring method should be used
+                    {
+                        if (iterations < MAX_ITERATIONS)  //Smooth coloring method
+                        {
+                            //Calculating z for 2 more iterations
+                            zr = (zr * zr) - (zi * zi) + cr; zrPrevious = zr; zi = (2 * zrPrevious * zi) + ci;
+                            zr = (zr * zr) - (zi * zi) + cr; zrPrevious = zr; zi = (2 * zrPrevious * zi) + ci;
+
+                            iterations += 2;
+
+                            mu = iterations - ((Math.Log(Math.Log(modulus))) / 2);
+                            colorIndex = (int)(mu / MAX_ITERATIONS * 768);
+                            if (colorIndex >= 768) colorIndex = 0;
+                            if (colorIndex < 0) colorIndex = 0;
+                        }
+                        pointerToFirstPixel[y * width + x] = colorArray[colorIndex];
+                    }
+                    else
+                    {
+                        if (iterations < MAX_ITERATIONS & modulus < 2)
+                        {
+                            RGBA[0] = (byte)((int)(modulus * 10) ^ 3 % 255);
+                            //Green is always zero
+                            RGBA[2] = 255;
+                        }
+                        else
+                        {
+                            RGBA[0] = 0;
+                            RGBA[2] = 0;
+                        }
+                        pointerToFirstPixel[y * width + x] = BitConverter.ToInt32(RGBA, 0);
+                    }
+
+                    maxR = (zr > maxR) ? zr : maxR;
+                    maxI = (zi > maxI) ? zi : maxI;
+                }
+            }
+
+            fractalBMP.UnlockBits(fractalBMPData);
+            //MessageBox.Show(zr.ToString() + "\n" + zi.ToString());
+            return fractalBMP;
+        }
+
+        private Bitmap drawColorPalatte()
+        {  //Drawing the palatte
+            Bitmap BMP = new Bitmap(768, height);
+            Graphics g = Graphics.FromImage(BMP);
+
+            Pen myPen;
+
+            for (int x = 0; x < 768; x++)
+            {
+                myPen = new Pen(new SolidBrush(Color.FromArgb(colorArray[x])));
+                g.DrawLine(myPen, x, 0, x, height);  //One vertical line for each color
+            }
+
+            return BMP;
+        }
+
+        private void btnShowPalatte_Click(object sender, EventArgs e)
+        {
+            //Draws the color palatte that is used by the smooth coloring method
+            updateHistory();
+            pictureBox1.Image = drawColorPalatte();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblCoordinates.Text = "Screen coordinates\n" + "X = " + e.X.ToString() + "  |  Y = " + e.Y.ToString() + "\n\nComplex plane coordinates\n" +
+                          "X = " + Math.Round(realSlope * e.X + xBound, 2).ToString() + "  |  Im = " + Math.Round(imaginarySlope * e.Y + yBound, 2).ToString();
+        }
+
+        private void btnDrawOriginal_Click(object sender, EventArgs e)
+        {
+            //Drawing the original MadelbrotSet
+            updateHistory();
+            zoom = 1;
+            xBound = -2.5;
+            yBound = 1.5d;
+            pictureBox1.Image = drawFractal();
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Zooming in
+            updateHistory();
+            zoom *= 10;
+            xBound = (realSlope * (e.X - (Width / 20)) + xBound);
+            yBound = (imaginarySlope * (e.Y - (Height / 20)) + yBound);
+
+            pictureBox1.Image = drawFractal();
+        }
+
+        private void updateHistory()
+        {
+            if (offset < 100)
+            {
+                //Saving all the variables in history variables for when needed for undoing
+                xBoundHistory[offset] = xBound;
+                yBoundHistory[offset] = yBound;
+                zoomHistory[offset] = zoom;
+                offset++;
+            }
+
+
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            if (offset > 0)
+            {
+                offset -= (sbyte)1;  //Going to the previous step
+
+                zoom = zoomHistory[offset];
+                xBound = xBoundHistory[offset];
+                yBound = yBoundHistory[offset];
+
+                pictureBox1.Image = drawFractal();
+
+            }
+        }
+
+        private void btnFullscreen_Click(object sender, EventArgs e)
+        {
+            bool visibility;
+
+            if (fullscreen == true)  //If fullscreen is enabled, disable it
+            {
+                width = 800; height = 600;
+                fullscreen = false;
+                visibility = true;
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                pictureBox1.Location = new Point(13, 13);
+                this.Location = new Point(50, 50);
+                pictureBox1.Size = new Size(width, height);
+                this.Size = new Size(843, 747);
+            }
+            else  //If fullscreen is disabled, enable it
+            {
+                width = SystemInformation.VirtualScreen.Width; height = SystemInformation.VirtualScreen.Height;
+                fullscreen = true;
+                visibility = false;
+                this.FormBorderStyle = FormBorderStyle.None;
+                pictureBox1.Location = new Point(0, 0);
+                this.Location = new Point(0, 0);
+                pictureBox1.Size = new Size(width, height);
+                this.Size = new Size(width, height);
+            }
+            btnShowPalatte.Visible = visibility;
+            btnDrawOriginal.Visible = visibility;
+            btnUndo.Visible = visibility;
+            btnFullscreen.Visible = visibility;
+            btnRefresh.Visible = visibility;
+
+            lblCoordinates.Visible = visibility;
+            lblZoom.Visible = visibility;
+
+            label1.Visible = visibility;
+
+            cmbBoxColoringMethods.Visible = visibility;
+
+            //Showing complex plane coordinates does not work until real and imaginary slopes are recalclated
+            realSlope = 4.0d / zoom / width;  //Recalculating real slope
+            imaginarySlope = -3.0d / zoom / height;  //Recalculating imaginary slope
+
+            pictureBox1.Image = drawFractal();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)  //Using keyboard controls when its on full screen
+        {
+            const int WM_KEYDOWN = 0x100;  //Code for key down event
+
+            if (msg.Msg == WM_KEYDOWN)
+            {
+                switch (keyData.ToString())
+                {
+                    case "Left":
+                        btnUndo_Click(null, null);  //Eventhandler mehtod of the Undo button
+                        break;
+
+                    case "Return":
+                        btnDrawOriginal_Click(null, null);
+                        break;
+
+                    case "Escape":
+                        btnFullscreen_Click(null, null);
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            return true;
+            // return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)  //Purpose of the refresh button is to view the same picture with a different coloring method
+        {
+            pictureBox1.Image = drawFractal();
+        }
+
+        private void btnShowControls_Click(object sender, EventArgs e)  //Show keyboard controls
+        {
+            const string controlsMessage = "Draw original fractal = Return\nUndo = Left Arrow\nExit fullscreen = Escape\n";
+            MessageBox.Show(controlsMessage, "Controls");
+        }
+    }
+}
